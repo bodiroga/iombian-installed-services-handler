4c6bac05bbb1498ae06eb27017022be9f8f92a5d
Aitor Iturrioz <aiturrioz@tknika.eus>
2024-08-14 17:06:54 +0200

    Add new 'status' based service handling
    This service has now two operation modes:
    - Offline: the "compose up" and "compose down" commands are executed based on filesystem changes
    - Online: the docker services are controlled using the "status" field stored in Firebase
    
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- src/iombian_services_handler.py
+++ src/iombian_services_handler.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pathlib
+import threading
 from typing import List
 
 from watchdog.events import (
@@ -31,38 +32,93 @@ class IombianServicesHandler(FileSystemEventHandler):
     """The `Observer` of the "iombian-services" folder"""
     services: List[InstalledServiceHandler]
     """List of the `InstalledServiceHandler` on the "iombian-services" folder"""
+    local_service_state_change_callback: callable
+    """Callback function to be called when a service state changes"""
+    mode: str
+    """Mode of the installation process: 'online' (based on Firebase) or 'offline' (based on folder events)"""
 
-    def __init__(self, base_path: str, wait_seconds: float) -> None:
+    def __init__(self, base_path: str, wait_seconds: float, local_service_state_change_callback: callable=lambda _: None):
         self.base_path = base_path
         self.wait_seconds = wait_seconds
-        self.observer = Observer()
+        self.local_service_state_change_callback = local_service_state_change_callback
+        self.observer = None
+        self.mode = "offline"
         self.services = []
+
+    def set_mode(self, mode: str):
+        """Set the handler mode: 'online' or 'offline'"""
+        logger.debug(f"Setting mode to '{mode}'")
+        self.mode = mode
 
     def start(self):
         """Start the handler by starting the observer of the "iombian-services" folder."""
-        logger.info("IoMBian Installed Services Handler started.")
-        self.observer.schedule(self, self.base_path)
-        self.observer.start()
+        logger.debug(f"IoMBian Services Handler started.")
+        if self.mode == "offline":
+            self.observer = Observer()
+            self.observer.schedule(self, self.base_path)
+            self.observer.start()
 
     def stop(self):
         """Stop the handler by stopping the observer of the "iombian-services" folder.
 
         Also stop all of the services in the "iombian-services" folder, but not the composes.
         """
-        logger.info("IoMBian Installed Services Handler stopped.")
-        self.observer.stop()
+        logger.debug("IoMBian Services Handler stopped.")
+        if self.observer:
+            self.observer.stop()
         for service in self.services:
             service.stop()
 
     def read_local_services(self):
         """Read the services in "iombian-services", start them and add them to the `services` list."""
+        logger.debug("Reading local services")
         self.services = []
         service_names = os.listdir(self.base_path)
+        logger.debug(f"{len(service_names)} local services detected")
         for service_name in service_names:
             service_path = f"{self.base_path}/{service_name}"
-            service = InstalledServiceHandler(service_path, self.wait_seconds)
-            service.start()
+            service = InstalledServiceHandler(
+                service_path, self.wait_seconds, self.on_local_service_state_changed)
+            service.start(self.mode)
             self.services.append(service)
+
+    def start_service(self, service_name: str):
+        """Start a service by it's name: up
+        
+        Method exposed so that external programs can interact with the services.
+        """
+        service = self._get_service_by_name(service_name)
+        if service:
+            service.reload_compose_services()
+            return
+        service_path = "/".join([self.base_path, service_name])
+        service = InstalledServiceHandler(
+            service_path, self.wait_seconds, self.on_local_service_state_changed)
+        service.up_compose_services()
+        service.start(self.mode)
+        self.services.append(service)
+
+    def reconfigure_service(self, service_name: str):
+        """Reconfigure a service by it's name: stop and up
+        
+        Method exposed so that external programs can interact with the services.
+        """
+        service = self._get_service_by_name(service_name)
+        if service:
+            service.stop_compose_services()
+            service.up_compose_services()
+
+    def delete_service(self, service_name: str):
+        """delete a service by it's name: stop, down and clean
+        
+        Method exposed so that external programs can interact with the services.
+        """
+        service = self._get_service_by_name(service_name)
+        if service:
+            service.stop()
+            service.down_compose_services(remove_images=True)
+            service.clean_compose_services()
+            self.services.remove(service)
 
     def on_created(self, event: FileSystemEvent):
         """When a new service is added to "iombian-services", start the service and the compose of the services.
@@ -75,9 +131,10 @@ class IombianServicesHandler(FileSystemEventHandler):
         service_path = event.src_path
         service_name = pathlib.Path(service_path).stem
         logger.debug(f"{service_name} service folder was created.")
-        service = InstalledServiceHandler(service_path, self.wait_seconds)
-        service.up()
-        service.start()
+        service = InstalledServiceHandler(
+            service_path, self.wait_seconds, self.on_local_service_state_changed)
+        service.up_compose_services()
+        service.start(self.mode)
         self.services.append(service)
         logger.debug(f"{service_path} service added.")
 
@@ -94,9 +151,24 @@ class IombianServicesHandler(FileSystemEventHandler):
         service = self._get_service_by_name(service_name)
         if service:
             service.stop()
-            service.down()
+            service.clean_compose_services()
             self.services.remove(service)
             logger.debug(f"{service_name} service removed.")
+
+    def on_local_service_state_changed(self, service_name: str, new_state: str):
+        """Called when a local service changes it's state.
+
+        :param service_name:
+            Name of the service that changed it's state.
+        :type event:
+            :class:`str`
+        :param new_state:
+            New state of the service.
+        :type event:
+            :class:`str`
+        """
+        if self.local_service_state_change_callback:
+            threading.Thread(target=self.local_service_state_change_callback, args=[service_name, new_state]).start()
 
     def _get_service_by_name(self, service_name: str):
         """Given the service name, return the service in "iombian-services"."""
--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import logging
 import os
 import signal
 
-import sdnotify
-
+from communication_module import CommunicationModule
+from installed_services_remote_provider import InstalledServicesRemoteProvider
 from iombian_services_handler import IombianServicesHandler
 
+CONFIG_HOST = os.environ.get("CONFIG_HOST", "127.0.0.1")
+CONFIG_PORT = int(os.environ.get("CONFIG_PORT", 5555))
 BASE_PATH = os.environ.get("BASE_PATH", "/opt/iombian-services")
 WAIT_SECONDS = int(os.environ.get("WAIT_SECONDS", 1))
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
@@ -16,16 +18,76 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def on_remote_connection_state_changed(state: str):
+    logger.debug(f"New remote connection state: {state}")
+    if state == "connected":
+        iombian_services_handler.set_mode("online")
+        iombian_services_handler.read_local_services()
+        iombian_services_handler.start()
+    elif state == "timeout":
+        logger.warning("The remote connection timeout, start the services handler in 'offline' mode")
+        iombian_services_handler.read_local_services()
+        iombian_services_handler.start()
+
+
+def on_local_service_state_changed(service_name: str, new_state: str):
+    logger.debug(f"New state for local service: {service_name}: {new_state}")
+    if iombian_services_remote_provider:
+        iombian_services_remote_provider.update_remote_service_status(service_name, new_state)
+
+
+def on_remote_service_state_changed(service_name: str, new_state: str):
+    logger.debug(f"New state for remote service: {service_name}: {new_state}")
+    if new_state == "downloaded":
+        logger.info(f"'{service_name}' service downloaded, starting it...")
+        iombian_services_handler.start_service(service_name)
+    elif new_state == "reconfigured":
+        logger.info(f"'{service_name}' service reconfigured, starting it...")
+        iombian_services_handler.reconfigure_service(service_name)
+    elif new_state == "updated":
+        logger.info(f"'{service_name}' service updated, starting it...")
+        iombian_services_handler.start_service(service_name)
+    elif new_state == "to-be-updated":
+        # Stopping the service and removing the images is needed before the old files are removed
+        logger.info(f"'{service_name}' service to be updated, starting it...")
+        iombian_services_handler.delete_service(service_name)
+    elif new_state == "to-be-uninstalled":
+        logger.info(f"'{service_name}' service to be uninstalled, stopping it...")
+        iombian_services_handler.delete_service(service_name)
+        if iombian_services_remote_provider:
+            iombian_services_remote_provider.update_remote_service_status(service_name, "to-be-removed")
+
+
 def signal_handler(sig, frame):
+    logger.info("Stopping IoMBian Installed Services Handler Service")
     iombian_services_handler.stop()
+    if iombian_services_remote_provider:
+        iombian_services_remote_provider.stop()
 
 
 if __name__ == "__main__":
-    iombian_services_handler = IombianServicesHandler(BASE_PATH, WAIT_SECONDS)
-    iombian_services_handler.read_local_services()
-    iombian_services_handler.start()
-    notifier = sdnotify.SystemdNotifier()
-    notifier.notify("READY=1")
+    logger.info("Starting IoMBian Installed Services Handler Service")
+
+    comm_module = CommunicationModule(host=CONFIG_HOST, port=CONFIG_PORT)
+    comm_module.start()
+
+    api_key = str(comm_module.execute_command("get_api_key"))
+    project_id = str(comm_module.execute_command("get_project_id"))
+    refresh_token = str(comm_module.execute_command("get_refresh_token"))
+    device_id = str(comm_module.execute_command("get_device_id"))
+
+    iombian_services_handler = IombianServicesHandler(
+            BASE_PATH, WAIT_SECONDS, on_local_service_state_changed)
+    
+    if not (api_key and project_id and refresh_token and device_id):
+        logger.warning(
+            "Wasn't able to get the necessary information from the config file handler"
+        )
+        iombian_services_handler.read_local_services()
+        iombian_services_handler.start()
+    else:
+        iombian_services_remote_provider = InstalledServicesRemoteProvider(api_key, project_id, refresh_token, device_id, on_remote_connection_state_changed, on_remote_service_state_changed)
+        iombian_services_remote_provider.start(timeout=30)
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
